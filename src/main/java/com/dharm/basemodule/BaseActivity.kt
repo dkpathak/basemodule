a6242c9386c7d08ee9e67545d8cc65f2534781ea
dharmenda <dk.iitism@gmail.com>
2022-03-23 17:28:04 +0530

    first commit.

---
+++ src/main/java/com/dharm/basemodule/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.dharm.basemodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+const val TAG = "BaseActivity"
+open class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i(TAG,"OnCreate Called")
+    }
+
+
+}
